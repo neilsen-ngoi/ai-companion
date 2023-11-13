3f0d92bcf2c5510b639ddfdf0e6cc09b3bb194db
neilsen-ngoi <kenisu222@gmail.com>
2023-11-13 18:02:27 +1300

    load categories

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,13 @@
 import SearchInput from '@/components/search-input'
+import prismadb from '@/lib/prismadb'
+import { Categories } from '@/components/Categories'
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany()
   return (
     <div className=" h-full p4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   )
 }
