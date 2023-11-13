2dab370ed06f38c69f74ba5c855a360f1339634f
neilsen-ngoi <kenisu222@gmail.com>
2023-11-13 15:49:37 +1300

    what is debounced

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,13 +1,18 @@
 'use client'
-
+import { Search } from 'lucide-react'
 import { Input } from './ui/input'
 import { SearchIcon } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 const SearchInput = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const categoryId = searchParams.get('categoryId')
+  const name = searchParams.get('name')
   return (
     <div className="relative">
       <SearchIcon className="absolute h-4 top-3 left-4 text-muted-foreground" />
-      <Input />
+      <Input placeholder="Search..." className=" pl-10 bg-primary/10" />
     </div>
   )
 }
