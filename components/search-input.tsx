555c52e1cc0be2ed74a5de52c5030d4ac62bf63d
neilsen-ngoi <kenisu222@gmail.com>
2023-11-13 15:36:54 +1300

    2

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,7 +1,15 @@
 'use client'
 
+import { Input } from './ui/input'
+import { SearchIcon } from 'lucide-react'
+
 const SearchInput = () => {
-  return <div>SearchInput</div>
+  return (
+    <div className="relative">
+      <SearchIcon className="absolute h-4 top-3 left-4 text-muted-foreground" />
+      <Input />
+    </div>
+  )
 }
 
 export default SearchInput
