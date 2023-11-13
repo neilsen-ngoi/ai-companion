93403a346e3822fe12fcd397bf55205e1e631348
neilsen-ngoi <kenisu222@gmail.com>
2023-11-13 16:13:06 +1300

    prisma

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -3,16 +3,47 @@ import { Search } from 'lucide-react'
 import { Input } from './ui/input'
 import { SearchIcon } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { ChangeEventHandler, useEffect, useState } from 'react'
+import { useDebounce } from '@/hooks/use-debounce'
+import qs from 'query-string'
 
 const SearchInput = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const categoryId = searchParams.get('categoryId')
   const name = searchParams.get('name')
+  const [value, setValue] = useState(name || '')
+  const debouncedValue = useDebounce<string>(value, 500)
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value)
+  }
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId: categoryId,
+    }
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      // remove the following from query
+      { skipEmptyString: true, skipNull: true }
+    )
+    router.push(url)
+  }, [debouncedValue, router, categoryId])
+
   return (
     <div className="relative">
       <SearchIcon className="absolute h-4 top-3 left-4 text-muted-foreground" />
-      <Input placeholder="Search..." className=" pl-10 bg-primary/10" />
+      <Input
+        onChange={onChange}
+        value={value}
+        placeholder="Search..."
+        className=" pl-10 bg-primary/10"
+      />
     </div>
   )
 }
