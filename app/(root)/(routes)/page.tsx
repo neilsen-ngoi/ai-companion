efacf29f467344a51941e354640bb94049f48026
neilsen-ngoi <kenisu222@gmail.com>
2023-11-15 12:22:56 +1300

    add card from shadcn & companion page

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,45 @@
 import SearchInput from '@/components/search-input'
 import prismadb from '@/lib/prismadb'
 import { Categories } from '@/components/Categories'
+import Companions from '@/components/Companions'
 
-const RootPage = async () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string
+    name: string
+  }
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+  // extract params from url to fetch companions based on filter
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        //full text search
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    //count all messages companion has
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  })
+
   const categories = await prismadb.category.findMany()
+
   return (
     <div className=" h-full p4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   )
 }
---
+++ components/Companions.tsx
@@ -0,0 +1,26 @@
+import { Companion } from '@prisma/client'
+import Image from 'next/image'
+
+interface CompanionsProps {
+  data: (Companion & {
+    _count: {
+      messages: number
+    }
+  })[]
+}
+
+const Companions = ({ data }: CompanionsProps) => {
+  if (data.length === 0) {
+    return (
+      <div className=" pt-10 flex flex-col justify-center items-center space-y-3">
+        <div className="relative w-60 h-60">
+          <Image fill className=" grayscale" alt="empty" src="/empty.png" />
+        </div>
+        <p className=" text-sm text-muted-foreground">No companions found</p>
+      </div>
+    )
+  }
+  return <div>Companions</div>
+}
+
+export default Companions
