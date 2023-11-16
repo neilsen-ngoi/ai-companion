0d80b36299ee0b1e938a44ca66c3bfbf077644c4
neilsen-ngoi <kenisu222@gmail.com>
2023-11-16 18:46:51 +1300

    commit

---
+++ hooks/use-pro-modal.tsx
@@ -0,0 +1,14 @@
+import { boolean } from 'zod'
+import { create } from 'zustand'
+
+interface useProModalStore {
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+}
+
+export const useProModal = create<useProModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}))
